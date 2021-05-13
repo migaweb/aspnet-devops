234801a9411d48fbf9ae72c1719d23ffd77c7a49
Mikael Storman Gustavsson <miga02@gmail.com>
2021-05-13 13:38:20 +0200

    added mongo connection

--- Shopping/Shopping.API/Controllers/ProductController.cs
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using Shopping.API.Data;
 using Shopping.API.Models;
 using System;
@@ -14,21 +15,22 @@ namespace Shopping.API.Controllers
   [ApiController]
   public class ProductsController : ControllerBase
   {
-    //private readonly ProductContext _context;
+    private readonly ProductContext _context;
     private readonly ILogger<ProductsController> _logger;
 
-    public ProductsController(ILogger<ProductsController> logger) //, ProductContext context)
+    public ProductsController(ILogger<ProductsController> logger, ProductContext context)
     {
-      //_context = context ?? throw new ArgumentNullException(nameof(context));
+      _context = context ?? throw new ArgumentNullException(nameof(context));
       _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
     [HttpGet]
     public async Task<IEnumerable<Product>> Get()
     {
-
-      return ProductContext.Products.ToList();
-                      
+      return await _context
+                            .Products
+                            .Find(p => true)
+                            .ToListAsync();
     }
 
   }
