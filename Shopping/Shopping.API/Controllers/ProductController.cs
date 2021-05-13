f719fa1290c98845d3b95538e489800c5f394673
Mikael Storman Gustavsson <miga02@gmail.com>
2021-05-13 11:19:02 +0200

    added shopping api

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class ProductsController : ControllerBase
+  {
+    //private readonly ProductContext _context;
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(ILogger<ProductsController> logger) //, ProductContext context)
+    {
+      //_context = context ?? throw new ArgumentNullException(nameof(context));
+      _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Product>> Get()
+    {
+
+      return ProductContext.Products.ToList();
+                      
+    }
+
+  }
+}
--- Shopping/Shopping.Client/Controllers/HomeController.cs
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Shopping.Client.Data;
 using Shopping.Client.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Shopping.Client.Controllers
@@ -13,15 +15,20 @@ namespace Shopping.Client.Controllers
   public class HomeController : Controller
   {
     private readonly ILogger<HomeController> _logger;
+    private readonly HttpClient _httpClient;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
     {
       _logger = logger;
+      _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-      return View(ProductContext.Products);
+      var response = await _httpClient.GetAsync("/products");
+      var content = await response.Content.ReadAsStringAsync();
+      var productsList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+      return View(productsList);
     }
 
     public IActionResult Privacy()
