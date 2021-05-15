a37d04f606e78fcf417f33c86c6b3f06cabfbd44
Mikael Storman Gustavsson <miga02@gmail.com>
2021-05-15 15:01:16 +0200

    Trigger pipeline

--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -36,7 +36,7 @@ namespace Shopping.API.Data
             {
                 new Product()
                 {
-                    Name = "IPhone X",
+                    Name = "IPhone X Trigger",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-1.png",
                     Price = 950.00M,
